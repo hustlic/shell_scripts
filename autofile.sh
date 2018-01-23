ac4b300fe79bef06aa285627f312cc065a771389
LiCheng <licheng@pset.suntec.net>
2018-01-23 17:02:44 +0800

    Add autofile.sh. This file will auto generate files with filename increasing

---
+++ autofile.sh
@@ -0,0 +1,16 @@
+#! /bin/sh
+
+i=0
+quit=-1
+
+while [ $quit -ne 0 ]
+do
+        if [ ! -f "dmesg_$i.txt" ]; then
+			touch dmesg_$i.txt
+            quit=0
+        else
+           i=`expr $i + 1`
+        fi
+done
+
+echo "dmesg_$i.txt created!"
